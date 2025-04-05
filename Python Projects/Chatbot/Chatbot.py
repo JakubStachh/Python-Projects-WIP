c4c5a10a32826842295fae09f9a2036d45f24b12
Jakub🐕 <166159581+JakubStachh@users.noreply.github.com>
2025-04-05 06:53:45 +0200

    Delete Python Projects/Chatbot directory

--- Python Projects/Chatbot/Chatbot.py
+++
@@ -1,8 +0,0 @@
-from chatterbot import ChatBot
-from chatterbot.trainers import ChatterBotCorpusTrainer
-
-chatbot = ChatBot("Bot")
-trainer = ChatterBotCorpusTrainer(chatbot)
-trainer.train("chatterbot.corpus.english")
-
-print(chatbot.get_response("Hello"))
